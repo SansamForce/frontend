76aaf2dcb6113ca216fa88351cd535087ff3c9e6
kookong2 <hy010309@naver.com>
2024-10-20 02:09:53 +0900

    [REFACTOR] TestComponent 적용

---
+++ frontend/src/components/TestComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import { onMounted } from "vue";
+    onMounted(() => {
+      console.log('Hello Vue!');
+    });
+
+</script>
+
+<template>
+  <div>
+    <h1>TestComponent</h1>
+  </div>
+</template>
+
+<style scoped>
+  h1 {
+    color : red;
+  }
+</style>
