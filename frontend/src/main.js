a9ea8b5e9809eda0f146ccfcccb99a5a3e41d55f
kookong2 <hy010309@naver.com>
2024-10-23 09:14:03 +0900

    [FEAT] pinia 라이브러리 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/tailwind.css';
 import BootstrapVue3 from 'bootstrap-vue-3';
+import { createPinia } from 'pinia';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
 const app = createApp(App);
+const pinia = createPinia();
+
 app.use(BootstrapVue3);
+app.use(pinia);
 app.mount('#app');
